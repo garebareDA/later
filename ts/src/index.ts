e025ec0ed27f63c26b7776954e6987e0adcfe9b4
garebare <garedaiki@outlook.jp>
2020-04-23 00:10:42 +0900

    fix

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,13 +1,17 @@
 'use strict';
 import Vue from 'vue';
 import Router from 'vue-router';
-import { default as home } from '../components/home.vue';
+import Vuetify from "vuetify";
+import 'vuetify/dist/vuetify.min.css';
+import {default as home } from '../components/home.vue';
 
 Vue.use(Router);
+Vue.use(Vuetify);
 const routes = [
   {path:'/', component:home},
 ];
 
+const vuetify = new Vuetify({});
 const router = new Router({
   routes
 });
@@ -15,4 +19,5 @@ const router = new Router({
 new Vue({
   el:"#app",
   router,
+  vuetify,
 })
--- ts/webpack.config.js
+++ ts/webpack.config.js
@@ -6,38 +6,55 @@ module.exports = {
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, '../static'),
-    filename:'index.js'
+    filename: 'index.js'
   },
   resolve: {
-    extensions: [ '.ts', '.js' ],
+    extensions: ['.ts', '.js'],
     modules: [path.resolve(__dirname, 'src'), 'node_modules'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
-    }
+    },
   },
   module: {
     rules: [
-        {
-            test: /\.ts$/,
-            exclude: /node_modules/,
-            loader: 'ts-loader',
-            options: {
-              appendTsSuffixTo: [/\.vue$/],
-            },
-          },
-
-          { test: /\.vue$/,
-            loader: 'vue-loader' },
-
-          {
-            test: /\.css$/,
-            use: [
-                "css-loader",
-            ],
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
         },
+      },
+
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+
+      {
+        test: /\.css$/,
+        use: ["css-loader"]
+      },
+
+      {
+        test: /\.s(c|a)ss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+              sassOptions: {
+                fiber: require('fibers'),
+              },
+            }
+          }
+        ]
+      }
     ]
   },
-  plugins:[
+  plugins: [
     new VueLoaderPlugin()
   ]
 }
