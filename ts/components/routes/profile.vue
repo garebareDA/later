bff57229b8753c1272a835d3737b09335b33f863
garebare <garedaiki@outlook.jp>
2020-04-24 18:39:49 +0900

    fix

--- ts/components/routes/profile.vue
+++ ts/components/routes/profile.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    
+    <v-content>
+      <v-container>
+        <v-card class="mx-auto" max-width="344">
+          <div>ユーザー名の変更</div>
+          <div>メールアドレスの変更</div>
+          <div>パスワードの変更</div>
+        </v-card>
+      </v-container>
+    </v-content>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 export default Vue.extend({
-  
-})
+
+});
 </script>
