c9e6c28db565ea8932ea093b9d4aca5c06dea29c
garebare <garedaiki@outlook.jp>
2020-04-30 21:19:30 +0900

    add:drafts

---
+++ ts/components/parts/drafts.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+        <v-content>
+      <v-container>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <v-card-text>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
